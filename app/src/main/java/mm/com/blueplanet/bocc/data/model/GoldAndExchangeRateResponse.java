057caf394c85d10df4c8121fad9756cf719bf4c8
Htoo Maung Thait bp <htoo.mt@blueplanet.com.mm>
2017-05-23 14:07:11 +0630

    Initial commit

--- app/src/main/java/mm/com/blueplanet/bocc/data/model/GoldAndExchangeRateResponse.java
+++ app/src/main/java/mm/com/blueplanet/bocc/data/model/GoldAndExchangeRateResponse.java
@@ -1,9 +1,56 @@
 package mm.com.blueplanet.bocc.data.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by smmon on 5/23/17.
  */
 
 public class GoldAndExchangeRateResponse {
+    @SerializedName("page")
+    private int page;
 
+    @SerializedName("results")
+    private List<BeautyTip> results;
+
+    @SerializedName("total_result")
+    private int totalResults;
+
+    @SerializedName("total_pages")
+    private int totalPages;
+
+
+    public int getPage() {
+        return page;
+    }
+
+    public List<BeautyTip> getResults() {
+        return results;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setResults(List<BeautyTip> results) {
+        this.results = results;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
 }
