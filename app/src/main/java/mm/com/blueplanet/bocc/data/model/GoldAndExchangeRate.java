57ce903bd3ee0d5c9668d7b23a23724dc986f5fb
Htoo Maung Thait bp <htoo.mt@blueplanet.com.mm>
2017-05-23 13:54:35 +0630

    News Module Starting at development branch

--- app/src/main/java/mm/com/blueplanet/bocc/data/model/GoldAndExchangeRate.java
+++ app/src/main/java/mm/com/blueplanet/bocc/data/model/GoldAndExchangeRate.java
@@ -1,8 +1,37 @@
 package mm.com.blueplanet.bocc.data.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by smmon on 5/23/17.
  */
 
 public class GoldAndExchangeRate {
+
+    @SerializedName("date")
+    private String date;
+
+    @SerializedName("message")
+    private  String message;
+
+    public GoldAndExchangeRate(String date, String message) {
+        this.date = date;
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
---
+++ app/src/main/java/mm/com/blueplanet/bocc/data/model/GoldAndExchangeRateResponse.java
@@ -0,0 +1,9 @@
+package mm.com.blueplanet.bocc.data.model;
+
+/**
+ * Created by smmon on 5/23/17.
+ */
+
+public class GoldAndExchangeRateResponse {
+
+}
